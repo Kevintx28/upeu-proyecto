8826ad6f81bf7e51e9e4bec72b022b017e112a46
Kevintx28 <>
2021-05-03 04:10:45 +0000

    Eje02

--- Act-1/Ejer01.java
+++ Act-1/Ejer01.java
@@ -4,8 +4,14 @@ class Ejer01{
 static Scanner input = new Scanner(System.in);
     public static void main(String[] args){
 int edad_de_la_persona;
+
+
+
 System.out.println("¿cual es su edad?");//imprimir la edad de la persona
-edad_de_la_persona=input.nextInt();//capturado calor asignado
+edad_de_la_persona=input.nextInt();//capturado valor asignado
+
+
+
 if (edad_de_la_persona>17) {
 System.out.println("si puede votar");
 }
